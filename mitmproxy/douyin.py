0472fcc9d1f61752259e47820fa00e6d0a2a3b4d
zhangxu0512 <shenhailuanma0512@163.com>
2018-09-21 15:56:36 +0800

    mitmproxy

--- mitmproxy/douyin.py
+++ mitmproxy/douyin.py
@@ -1,9 +1,11 @@
-import json
+import requests
 
 def request(flow):
     # print("zx get url:" + flow.request.url)
     url='http://v'
     #筛选出以上面url为开头的url
     if flow.request.url.startswith(url):
-        print("zx get video url:" + flow.request.url)
+        print("xxxxxxx get video url:" + flow.request.url)
+        # create download task
+        
 
